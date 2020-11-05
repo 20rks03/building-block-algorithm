140208b71d9599923a5095d99703bc770074f356
Kenneth Song <57800887+20rks03@users.noreply.github.com>
2020-11-05 23:28:15 +0900

    Copyright
    code from
    https://stackoverflow.com/questions/55166930/image-to-0-1-text

--- binaryimage.py
+++ binaryimage.py
@@ -2,3 +2,8 @@ from PIL import Image
 srcImage = Image.open("cube.jpg")
 grayImage = srcImage.convert('L')
 binarizedImage = grayImage.point(lambda x: 0 if x<128 else 255, '1')
+
+'''
+code from
+https://stackoverflow.com/questions/55166930/image-to-0-1-text
+'''
